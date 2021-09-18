1e97cb10f4b59ae3cdce7a19641b285604ed786d
syndersonLEE <winnery93@gmail.com>
2021-09-19 00:55:12 +0900

    init commit

---
+++ src/main/java/team/arton/coreserver/CoreserverApplication.java
@@ -0,0 +1,13 @@
+package team.arton.coreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreserverApplication.class, args);
+    }
+
+}
