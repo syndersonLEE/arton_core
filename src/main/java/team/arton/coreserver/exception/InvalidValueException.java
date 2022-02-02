de425c55eb25d78cee8f734e7b953beec2d4b7a2
syndersonLEE <winnery93@gmail.com>
2022-02-03 00:34:21 +0900

    add exception handler

--- src/main/java/team/arton/coreserver/exception/InvalidValueException.java
+++ src/main/java/team/arton/coreserver/exception/InvalidValueException.java
@@ -1,5 +1,9 @@
 package team.arton.coreserver.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class InvalidValueException extends BussinessException {
 
     public InvalidValueException(String errorType) {
