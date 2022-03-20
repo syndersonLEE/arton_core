547208a38ba3e91d44cd9347cfaffcdeaad6aa04
syndersonLEE <winnery93@gmail.com>
2022-03-20 11:18:03 +0900

    fix getHome to post

--- src/main/java/team/arton/coreserver/api/AdminController.java
+++ src/main/java/team/arton/coreserver/api/AdminController.java
@@ -1,9 +1,9 @@
 package team.arton.coreserver.api;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import team.arton.coreserver.common.auth.Auth;
 import team.arton.coreserver.common.auth.AuthContext;
 import team.arton.coreserver.domain.User;
@@ -33,10 +33,5 @@ public class AdminController {
         return DefaultResponse.res(StatusType.OK);
     }
 
-//    @ApiOperation("Admin 계정 등록")
-//    @PostMapping("/api/admin/signup")
-//    public DefaultResponse adminSignup(@RequestBody) {
-//
-//    }
 
 }
